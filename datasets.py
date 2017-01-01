aea8f6d6416cb35410d847d96d9b6e7c271c77c2
Will Monroe <wmonroe4@stanford.edu>
2017-01-01 16:50:14 -0600

    Add OpenSub data

--- datasets.py
+++ datasets.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from itertools import islice
 
 from stanza.research.instance import Instance
 from stanza.research.rng import get_rng
@@ -7,11 +8,56 @@ from stanza.research.rng import get_rng
 rng = get_rng()
 
 
-def dialogues_to_instances(dialogues):
-    return [Instance(input=' '.join(dialogue[max(0, i - 2):i]),
+def dialogues_to_instances(dialogues, context_len=2):
+    return [Instance(input=' '.join(dialogue[max(0, i - context_len):i]),
                      output=dialogue[i])
             for dialogue in dialogues
             for i in range(len(dialogue))]
+
+
+def dialogue_batches(data_file, dialogue_size=None, context_len=2, start=None, end=None):
+    insts = []
+    with open(data_file, 'r') as infile:
+        batch = []
+        for line in islice(infile, start, end):
+            batch.append(line)
+            if len(batch) >= dialogue_size:
+                insts.extend(dialogues_to_instances([batch], context_len=context_len))
+                batch = []
+        insts.extend(dialogues_to_instances([batch], context_len=context_len))
+    return insts
+
+
+OPENSUB_SIZE = 88134110
+OPENSUB_TRAIN_SIZE = int(OPENSUB_SIZE * 0.8)
+
+
+def opensub_train():
+    return dialogue_batches('data/opensub/2012.data',
+                            dialogue_size=1000,
+                            context_len=2,
+                            start=0, end=OPENSUB_TRAIN_SIZE)
+
+
+def opensub_test():
+    return dialogue_batches('data/opensub/2012.data',
+                            dialogue_size=1000,
+                            context_len=2,
+                            start=OPENSUB_TRAIN_SIZE, end=None)
+
+
+def opensub5k_train():
+    return dialogue_batches('data/opensub/2012_5k.data',
+                            dialogue_size=500,
+                            context_len=2,
+                            start=0, end=2500)
+
+
+def opensub5k_test():
+    return dialogue_batches('data/opensub/2012_5k.data',
+                            dialogue_size=500,
+                            context_len=2,
+                            start=2500, end=None)
 
 
 def toy_train():
@@ -45,4 +91,6 @@ DataSource = namedtuple('DataSource', ['train_data', 'test_data'])
 
 SOURCES = {
     'toy': DataSource(toy_train, toy_test),
+    '5k': DataSource(opensub5k_train, opensub5k_test),
+    'opensub': DataSource(opensub_train, opensub_test),
 }
